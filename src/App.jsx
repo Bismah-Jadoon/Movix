aad36389a1ed4f62ec4b9694211f0993888b28ff
Bismah-Jadoon <khanbismah63@gmail.com>
2024-03-05 16:23:10 +0500

    project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
-
-
-  return (
-    <div className='App'>App</div>
-  )
+  return <div className="App">App</div>;
 }
 
-
-
-
-export default App
+export default App;
