80e2f6a76c218f52661b4a709fae2d20bfaec93c
Bismah-Jadoon <khanbismah63@gmail.com>
2024-03-15 09:21:12 +0500

    project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from 'react-redux'
-
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
+ const dispatch = useDispatch()
+
   useEffect(() => {
     apiTesting();
   }, []);
@@ -11,6 +13,7 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getApiConfiguration(res));
     });
   };
 
