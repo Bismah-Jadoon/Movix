2d23c00f910738521202d363e5910f3d7460cfd2
Bismah-Jadoon <khanbismah63@gmail.com>
2024-03-15 10:06:54 +0500

    pro

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration } from "./store/homeSlice";
 
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details"
+import SearchResult from "./pages/searchResult/SearchResult"
+import Explore from "./pages/explore/Explore"
+import pageNotFound from "./pages/404/pageNotFound"
+
 function App() {
  const dispatch = useDispatch()
  const {url} = useSelector((state) => state.home)
@@ -20,6 +28,7 @@ function App() {
   };
 
   return <div className="App">App
+  
   {url?.total_pages} 
   </div>;
 }
