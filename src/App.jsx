84372132f668aa380f0ecf973dcff33f2580ca9d
Bismah-Jadoon <khanbismah63@gmail.com>
2024-03-15 09:14:05 +0500

    project

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from 'react-redux'
+
 
 function App() {
   useEffect(() => {
     apiTesting();
   }, []);
 
-
   const apiTesting = () => {
-    fetchDataFromApi('/movie/popular').then((res) => {
+    fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
     });
   };
 
-
-
-
-
-
-
-  
   return <div className="App">App</div>;
-
 }
 
 export default App;
