337526af0a9f11ad9c916305c6a17bf10d931fb7
Bismah-Jadoon <khanbismah63@gmail.com>
2024-03-15 09:25:43 +0500

    project

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
  const dispatch = useDispatch()
+ const {url} = useSelector((state) => state.url)
 
   useEffect(() => {
     apiTesting();
