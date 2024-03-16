fafee1ab4a3a8d3fb57a8ec12507b4118bc79e56
Bismah-Jadoon <khanbismah63@gmail.com>
2024-03-16 16:10:13 +0500

    pro

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -2,5 +2,19 @@ import React from "react";
 import "./style.scss";
 
 export default function HeroBanner() {
-  return <div>HeroBanner</div>;
+  return(
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+            <span className="title">Welcome.</span>
+            <span className="subtitle">Millions of movies, TV shows and people
+             to discover.
+            Explore now.</span>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+
+
