9d4514a1f654c0e7942eed2b8ffdd32241f3d039
Bismah-Jadoon <khanbismah63@gmail.com>
2024-04-12 11:48:38 +0500

    p

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,13 @@ function App() {
   const fetchApiConfig = () => {
     fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
+
+    const url = {
+      backdrop: res.images.secure_base_url + "original", 
+      poster: res.images.secure_base_url + "original", 
+      profile: res.images.secure_base_url + "original", 
+    }
+
       dispatch(getApiConfiguration(res));
     });
   };
